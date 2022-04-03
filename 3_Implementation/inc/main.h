73ffe35b149803b19c68bfd1e3a844cd50fc80f0
vinaykumarakula <101239900+vinaykumarakula@users.noreply.github.com>
2022-04-03 08:04:38 +0530

    Update main.h

--- 3_Implementation/inc/main.h
+++ 3_Implementation/inc/main.h
@@ -1,13 +1,4 @@
-/**
- * @file main.h
- * @author raaga.vardhini@ltts.com
- * @brief 
- * @version 0.1
- * @date 2021-09-08
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
